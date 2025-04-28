4f2ced0ee61d64ec6385c31ddcc9e18b595005d5
Danilo <danmedradocinema@gmail.com>
2025-04-28 20:59:44 -0300

    Exercicios da aula

--- Aula_04.py
+++ Aula_04.py
@@ -1,20 +1,36 @@
 #x = float(input("Insira um número para sequenciar: "))
-
+#Faça um programa que escreva de 1 a 100
 # x = 1
 # while x <= 100:
 #     print(x)
 #     #    x = x + 1 ____Isso pode ser escrito assim:
 #     x += 1
 
+#Faça um programa que escreva de 50 ate 100
 # x = 50
 # while x <= 100:
 #     print(x)
 #     x += 1
 
-x = 10
-print("Preparar para lançamento em...")
-while x >= 0:
-    print(x)
-    x -= 1
-print("FOGO!")
+#Faça um programa para contagem regressiva de um lançamento de um foguete
+# from time import sleep
+# x = 10
+# print("Preparar para lançamento em...")
+# while x >= 0:
+#     print(x)
+#     sleep(1)
+#     x -= 1
+# print("FOGO!")
 
+#Faça um programa que imprima os números impares do 1 até o número que o usuário escolher
+# fim = int(input("Digite o último número a imprimir: "))
+# x = 1
+# while x <= fim:
+#     print(x)
+#     x +=2
+
+#Faça um programa que mostre os 10 primeiros multiplos de 3
+x = 3
+while x <= 30:
+    print(x)
+    x +=3
