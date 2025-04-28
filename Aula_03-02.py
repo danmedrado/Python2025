82a4d9763a688601126f1b64f2085b5e1f81e217
Danilo <danmedradocinema@gmail.com>
2025-04-28 20:41:56 -0300

    Exercicios da aula

---
+++ Aula_03-02.py
@@ -0,0 +1,30 @@
+# fazer uma calculadora
+#
+# Num1 = float(input("Digite o primeiro número: "))
+# Num2 = float(input("Digite o segundo número: "))
+# variavel = input("Qual operação deseja fazer? Adição, subtração, multiplicação ou divisão? ")
+#
+# if variavel == "adição":
+#     print (Num1+Num2)
+# elif variavel == "subtração":
+#     print (Num1-Num2)
+# elif variavel == "multiplicação":
+#     print (Num1*Num2)
+# else:
+#     print (Num1/Num2)
+
+# fazer um programa de aprovação de crédito
+
+# ValorCasa = float(input("Qual valor do imóvel que deseja comprar? "))
+# Salario = float(input("Qual sua renda mensal? "))
+# Anos = int(input("Em quantos anos deseja financiar o imóvel? "))
+# Prestacao = ValorCasa/(Anos*12)
+#
+# if Prestacao > Salario*0.3:
+#     print("Você não pode comprar esse imóvel")
+# else:
+#     print(f"Para comprar esse imóvel você pagará R$ {Prestacao:.2f} durante {Anos*12} meses")
+
+
+
+
---
+++ Aula_04.py
@@ -0,0 +1,20 @@
+#x = float(input("Insira um número para sequenciar: "))
+
+# x = 1
+# while x <= 100:
+#     print(x)
+#     #    x = x + 1 ____Isso pode ser escrito assim:
+#     x += 1
+
+# x = 50
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+x = 10
+print("Preparar para lançamento em...")
+while x >= 0:
+    print(x)
+    x -= 1
+print("FOGO!")
+
